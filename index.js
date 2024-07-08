be3594807e6a1056f89be2ce5dba65ce2bd10bf8
Devi-Harshan <deviharshan97@gmail.com>
2024-07-08 17:50:16 -0400

    server added

---
+++ index.js
@@ -0,0 +1,8 @@
+import getPosts, { getPostsLength } from "./postController.js";
+
+console.log(getPosts(), getPostsLength());
+
+// const { generateRandomNumber, celsiusToFarenheit } = require("./utils");
+
+// console.log("Random", generateRandomNumber());
+// console.log("Celsius", celsiusToFarenheit(0));
---
+++ postController.js
@@ -0,0 +1,14 @@
+const posts = [
+  {
+    id: 1,
+    title: "Hello World",
+  },
+  {
+    id: 2,
+    title: "Hello World",
+  },
+];
+
+const getPosts = () => posts;
+export const getPostsLength = () => posts.length;
+export default getPosts;
