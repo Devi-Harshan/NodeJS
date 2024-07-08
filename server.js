4bc933d25cdab5f37724b1da2f3401f3b0dd2caa
Devi-Harshan <deviharshan97@gmail.com>
2024-07-08 18:01:10 -0400

    dependency added

--- server.js
+++ server.js
@@ -2,23 +2,10 @@ import http from "http";
 const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Type", "text/html");
   res.end("Hello World");
 });
 
 server.listen(PORT, () => {
   console.log("Server running in ", PORT);
 });
-
-// import http from "http";
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader("Content-Type", "text/plain");
-//   res.end("Hello World\n");
-// });
-
-// const PORT = 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}/`);
-// });
