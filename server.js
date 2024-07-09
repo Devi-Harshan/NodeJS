0e5258b2b69818cdd221d994488d4056c2ec5470
Devi-Harshan <deviharshan97@gmail.com>
2024-07-08 23:09:32 -0400

    node js server added

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 import http from "http";
-const PORT = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("Hello World");
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+const PORT = process.env.PORT;
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error("Not found");
+      }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/html" });
+  }
 });
 
 server.listen(PORT, () => {
   console.log("Server running in ", PORT);
 });
+
+// res.writeHead(200, { "Content-Type": "text/html" });
+//res.end("Hello World");
